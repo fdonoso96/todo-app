b695a61977adcd78fe836234179c5175e0e3ffa1
fdonoso <fran.donoso1996@gmail.com>
2023-01-11 19:40:32 -0600

    Implemented clearing GUI input box after item added

--- gui.py
+++ gui.py
@@ -37,6 +37,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case "Edit":
             try:
                 todo_to_edit = values['todos'][0]
